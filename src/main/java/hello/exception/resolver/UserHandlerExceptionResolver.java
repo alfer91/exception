a231e43a321921264b787ffe778d49033408abbe
Dann <ektjdektjd@gmail.com>
2022-05-03 16:30:38 +0900

    스프링이 제공하는 ExceptionResolver1

--- src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
+++ src/main/java/hello/exception/resolver/UserHandlerExceptionResolver.java
@@ -1,7 +1,7 @@
 package hello.exception.resolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import hello.exception.UserException;
+import hello.exception.exception.UserException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
